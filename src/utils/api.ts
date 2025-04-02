22665bde171d7928830b3fc3c1a95072e5efae0b
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2025-04-02 23:53:21 +0200

    sss

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import type { Product } from '../types/product';
 
-const API_URL = 'http://57.129.0.154/api/data';
+const API_URL = 'https://57.129.0.154/api/data';
 
 export async function fetchProducts(): Promise<Product[]> {
   try {
