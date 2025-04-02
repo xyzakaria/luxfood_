a812e77f4070c45765e38ce3395642575fb9fb87
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2025-04-03 01:56:15 +0200

    s

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import type { Product } from '../types/product';
 
-const API_URL = 'https://57.129.0.154/api/data';
+const API_URL = 'https://raw.githubusercontent.com/xyzakaria/luxfood_/refs/heads/main/src/data/data.json';
 
 export async function fetchProducts(): Promise<Product[]> {
   try {
