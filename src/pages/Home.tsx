01fd1876c0679d2ae2da3ff7e1535da96620615f
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2025-04-11 18:36:56 +0200

    qqq

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -173,7 +173,7 @@ export default function Home() {
       </div>
 
       {/* Featured Products Slider */}
-      <div className="bg-white  py-16">
+      <div className="bg-blue-300  py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-12">
             Featured Products
